cb95cba3639bfe422f6393d9ba647761a3a9c06f
Artem298 <artem.buyniy@gmail.com>
2026-06-19 17:38:01 +0200

    configured .gitignore & added custom BinOverflowError exception

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import List, Optional
+ 
+ 
+# --- CUSTOM EXCEPTIONS ---
+ 
 class BinOverflowError(Exception):
     """Raised when adding waste would exceed the bin's weight capacity."""
     pass
